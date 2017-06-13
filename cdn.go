4786a0896f0a840f1d28adf9c32c8df8704aab69
Peter Preeper <ppreeper@gmail.com>
2017-06-13 14:07:39 -0600

    split off cdn library

--- cdn.go
+++
@@ -1,6 +0,0 @@
-package cdn
-
-// DieSides Determine Die Sides from notation
-func DieSides(notation string) int {
-	return 6
-}
--- dice.go
+++ dice.go
@@ -1,10 +1,6 @@
 package main
 
-import (
-	"fmt"
-
-	_ "dice/cdn"
-)
+import "fmt"
 
 func main() {
 	fmt.Println("Dice")
