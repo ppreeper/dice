e512d85c9bb48746827c74472d86928e95332e56
Peter Preeper <ppreeper@gmail.com>
2017-06-13 17:06:48 -0600

    fold roll function into dice main package

--- dice.go
+++ dice.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+
+	"git.preeper.org/peterp/cdn"
+)
 
 func main() {
 	fmt.Println("Dice")
-	fmt.Println(roll.Roll(6))
+	fmt.Println(cdn.DieSides("1d6"))
+	fmt.Println(Roll(cdn.DieSides("1d6")))
+}
+
+//Roll soll the die given
+func Roll(sides int) int {
+	// rand.Seed(0)
+	return rand.Intn(sides)
 }
--- roll.go
+++
@@ -1,9 +0,0 @@
-package roll
-
-import "math/rand"
-
-//Roll soll the die given
-func Roll(sides int) int {
-	// rand.Seed(0)
-	return rand.Intn(sides)
-}
