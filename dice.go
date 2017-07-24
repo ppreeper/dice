c00bf2ecb2caa93766f49601d42c4a74d9482ab6
Peter Preeper <ppreeper@gmail.com>
2017-07-23 22:51:49 -0600

    add dice notation tests

--- dice.go
+++ dice.go
@@ -3,26 +3,70 @@ package main
 import (
 	"fmt"
 	"math/rand"
-
-	"git.preeper.org/peterp/cdn"
+	"regexp"
+	"strconv"
 )
 
-func main() {
-	fmt.Println("Dice")
-	fmt.Println(cdn.DieCount("1d6"))
-	fmt.Println(cdn.DieSides("1d6"))
-	fmt.Println(Roll(6))
-	fmt.Println(Roll(6))
-	fmt.Println(Roll(6))
-	fmt.Println(Roll(6))
-	fmt.Println(Roll(6))
-	fmt.Println(Roll(6))
+// Dice structure of the dice set
+type Dice struct {
+	DieType    string
+	DieModFunc string
+	DieCount   int
+	DieSides   string
+	DieModVal  int
+}
+
+// Pattern determine pattern from dice notation string
+func (d *Dice) Pattern(die string) {
+	dCount := regexp.MustCompile(`(\d+)d`)
+	sCount := regexp.MustCompile(`d(\d+)`)
+	modifier := regexp.MustCompile(`[x/+-]`)
+	modVal := regexp.MustCompile(`[x/+-](\d+)`)
+	sPattern := regexp.MustCompile(`^(\d+)?d(\d+)([x/+-](\d+))?`)
+	fudge := regexp.MustCompile(`^(\d+)F`)
+
+	fmt.Printf("\n%s\t", die)
+	if sPattern.MatchString(die) == true {
+		fmt.Printf("sPattern\t")
+		d.DieType = "d"
+		dc := dCount.FindSubmatch([]byte(die))
+		sc := sCount.FindSubmatch([]byte(die))
+		mdf := modifier.FindSubmatch([]byte(die))
+		mdv := modVal.FindSubmatch([]byte(die))
+		if len(dc) == 0 {
+			d.DieCount = 1
+		} else {
+			d.DieCount, _ = strconv.Atoi(string(dc[1]))
+		}
+		if len(mdf) == 0 {
+			d.DieModFunc = ""
+		} else {
+			d.DieModFunc = string(mdf[0])
+		}
+		if len(mdv) == 0 {
+			d.DieModVal = 0
+		} else {
+			d.DieModVal, _ = strconv.Atoi(string(mdv[1]))
+		}
+		d.DieSides = string(sc[1])
+	}
+	if fudge.MatchString(die) == true {
+		d.DieType = "F"
+		dc := fudge.FindSubmatch([]byte(die))
+		d.DieCount, _ = strconv.Atoi(string(dc[1]))
+	}
 }
 
 //Roll soll the die given
-func Roll(sides int) int {
+func (d *Dice) Roll(sides int) int {
 	// rand.Seed(0)
 	return rand.Intn(sides)
+}
+
+func main() {
+	fmt.Println("Dice")
+	// fmt.Println(cdn("1d6"))
+	// fmt.Println(cdn("1d6"))
 }
 
 //Mul multiply
--- dice_test.go
+++ dice_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 // use values that you know are right
 var mulTests = []struct {
@@ -39,4 +42,54 @@ func BenchmarkFibFunc(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_ = fn()
 	}
+}
+
+var di = []string{
+	"1d6", "2d6", "10d8",
+	"4F",
+	"d6", "d20",
+	"d6+2", "d20+5",
+	"3d20x5", "3d20/5",
+	"3d20+5", "3d20-5",
+}
+
+// use values that you know are right
+// var mulPatternTests = []struct {
+// 	a        string
+// 	expected Dice
+// }{
+// 	{"1d6", 1},
+// 	{"2d6", 1},
+// 	{"10d8", 1},
+// 	{"4F", 1},
+// 	{"d6", 1},
+// 	{"d20", 1},
+// 	{"d6+2", 1},
+// 	{"d20+5", 1},
+// 	{"3d20x5", 1},
+// 	{"3d20/5", 1},
+// 	{"3d20+5", 1},
+// 	{"3d20-5", 1},
+// }
+
+func TestPattern(t *testing.T) {
+	var d Dice
+	d.Pattern("d6")
+	fmt.Printf("DieType: %s", d.DieType)
+	fmt.Printf("\tDieCount: %d", d.DieCount)
+	if d.DieType != "d" {
+		t.Errorf("Pattern expected d")
+	}
+	d.Pattern("3d6")
+	fmt.Printf("DieType: %s", d.DieType)
+	fmt.Printf("\tDieCount: %d", d.DieCount)
+	d.Pattern("d6+20")
+	fmt.Printf("DieType: %s", d.DieType)
+	fmt.Printf("\tDieCount: %d", d.DieCount)
+	d.Pattern("3d6+20")
+	fmt.Printf("DieType: %s", d.DieType)
+	fmt.Printf("\tDieCount: %d", d.DieCount)
+	d.Pattern("4F")
+	fmt.Printf("DieType: %s", d.DieType)
+	fmt.Printf("\tDieCount: %d", d.DieCount)
 }
